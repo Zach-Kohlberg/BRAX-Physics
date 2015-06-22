7b619bcbe06e0583426f54557edd520372db6661
Ron M Burgess <ronaldmburgess@sbcglobal.net>
2015-06-22 00:25:51 -0400

    Platform
    Small PLatform script created for testing with Zach.

---
+++ Platform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : PhysicsObject {
+
+    public float time_Switch, spd_h;
+    private float time_SwitchUsed;
+    private int dir;
+
+	// Use this for initialization
+	void Start () {
+        dir = 1;
+        //time_Switch = 3f;
+        Speed_Horizontal = spd_h;
+        time_SwitchUsed = 0;
+        PhysicsObject_Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (time_SwitchUsed < time_Switch)
+        {
+            PhysicsObject_Move(dir, 0, false);
+
+            time_SwitchUsed += Time.deltaTime;
+            if (time_SwitchUsed >= time_Switch)
+            {
+                time_SwitchUsed = 0;
+                dir *= -1;
+            }
+        }
+        
+	}
+}
